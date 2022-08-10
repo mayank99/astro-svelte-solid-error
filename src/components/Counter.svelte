d272df9ffadb6f8e58e139ed46e60d352947646b
Mayank <mayank99@users.noreply.github.com>
2022-08-09 21:24:43 -0400

    Make it so

---
+++ src/components/Counter.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import AddIcon from './AddIcon.svelte';
+	import SubtractIcon from './SubtractIcon.svelte';
+
+	let count = 0;
+	const add = () => void (count += 1);
+	const subtract = () => void (count -= 1);
+</script>
+
+<div class="counter">
+	<button on:click={subtract}>
+		<SubtractIcon />
+	</button>
+	<pre>{count}</pre>
+	<button on:click={add}>
+		<AddIcon />
+	</button>
+</div>
+
+<style>
+	.counter {
+		display: grid;
+		font-size: 2em;
+		grid-template-columns: repeat(3, minmax(0, 1fr));
+		margin-top: 2em;
+		place-items: center;
+	}
+</style>
